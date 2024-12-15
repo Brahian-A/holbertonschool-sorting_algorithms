74e9c6e3ab87d82a2b0b1a4a602ca015c1c6f4c1
Emimoreira <10152@holbertonstudents.com>
2024-12-15 16:41:27 +0000

    commit_message

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,37 +7,37 @@
  * @array: Puntero al array a ordenar.
  * @size: El tamaño del array.
  */
-
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_index, k;
 	int temp;
 
-	for (i = 0; i < size - 1; i++) 
+	for (i = 0; i < size - 1; i++)
 	{
 	min_index = i;
-        
-	for (j = i + 1; j < size; j++) 
+
+	for (j = i + 1; j < size; j++)
 	{
-		if (array[j] < array[min_index]) 
+		if (array[j] < array[min_index])
 		{
 		min_index = j;
 		}
 	}
 
-	if (min_index != i) 
+	if (min_index != i)
 	{
 		temp = array[i];
 		array[i] = array[min_index];
 		array[min_index] = temp;
 
-		for (k = 0; k < size; k++) 
+		for (k = 0; k < size; k++)
 		{
-		printf("%d", array[k]);
-		if (k < size - 1)
-		printf(" ");
+			printf("%d", array[k]);
+			if (k < size - 1)
+			printf(", ");
 		}
 		printf("\n");
 	}
 	}
 }
+
