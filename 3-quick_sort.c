1d23b900a18a8130553f25832c81782f41df4f65
Brahian-A <10114@holbertonstudents.com>
2024-12-15 22:43:24 -0300

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,19 +10,20 @@ void swap(int *a, int *b)
 
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-	int random_pivot = low + rand() % (high - low + 1);
+	int random_pivot, pivot, i, j;
+	random_pivot = low + rand() % (high - low + 1);
 	swap(&array[random_pivot], &array[high]);
 	print_array(array, size);
 
-	int pivot = array[high];
-	int i = low - 1;
+	pivot = array[high];
+	i = low - 1;
 
-	for (int j = low; j < high; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
 		{
 			i++;
-			if (i != j);
+			if (i != j)
 			{
 				swap(&array[i], &array[j]);
 				print_array(array, size);
