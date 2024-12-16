f8aa47436d2be1b4fa2285486c70362d41c84482
Brahian-A <10114@holbertonstudents.com>
2024-12-15 22:32:07 -0300

    Create 3-quick_sort.c desde git hub móvil llevo una vida acá

---
+++ 3-quick_sort.c
@@ -0,0 +1,57 @@
+#include "sort.h"
+#include <stdlib.h>
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int random_pivot = low + rand() % (high - low + 1);
+	swap(&array[random_pivot], &array[high]);
+	print_array(array, size);
+
+	int pivot = array[high];
+	int i = low - 1;
+
+	for (int j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot = lomuto_partition(array, low, high, size);
+
+		quick_sort_recursive(array, low, pivot - 1, size);
+		quick_sort_recursive(array, pivot + 1, high, size);
+	}
+}
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
+}
